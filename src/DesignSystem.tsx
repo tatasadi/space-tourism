165d705a89d7889518cf173362566988d3fe04d8
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-11-05 11:21:58 +0100

    add nav design

--- src/DesignSystem.tsx
+++ src/DesignSystem.tsx
@@ -157,7 +157,27 @@ export default function DesignSystem() {
         </h2>
 
         {/* navigation  */}
-        <div></div>
+        <div>
+          <nav>
+            <ul className="primary-navigation underline-indicators flex">
+              <li className="active">
+                <a className="uppercase text-white letter-spacing-2" href="#">
+                  <span>00</span> Active
+                </a>
+              </li>
+              <li>
+                <a className="uppercase text-white letter-spacing-2" href="#">
+                  <span>01</span> Hovered
+                </a>
+              </li>
+              <li>
+                <a className="uppercase text-white letter-spacing-2" href="#">
+                  <span>02</span> Idle
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
         <div className="flex">
           <div style={{ marginTop: "5rem" }}>
